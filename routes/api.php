b95669c2e9384c72c5ee1a8cb3f52808119507b2
Ansel Melly <ansel@anselmelly.com>
2024-04-18 15:39:26 +0300

    API: remove from middleware

--- routes/api.php
+++ routes/api.php
@@ -17,28 +17,28 @@ use function Safe\date;
  * @date Apr 1, 2024
  * @link https://anselmelly.com
  */
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-        ], function () {
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
 
-            Route::post('login', [AuthController::class, 'login']);
-            Route::post('register', [AuthController::class, 'register']);
-            
-            Route::post('verify', [AuthController::class, 'verifyEmail']);
-            Route::post('logout', [AuthController::class, 'logout']);
-            Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
-            Route::post('reset-password', [AuthController::class, 'resetPassword']);
-            
-        });
-Route::group(['middleware' => 'jwt.auth'], function () {
-    Route::get('/profile', [AuthController::class, 'profile']);
-    Route::post('change-password', [AuthController::class, 'changePassword']);
-    Route::get('/creators', [CreatorsController::class, 'creators']);
-    Route::get('/creators/{id}', [CreatorsController::class, 'creator']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::post('verify', [AuthController::class, 'verifyEmail']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
+    Route::post('reset-password', [AuthController::class, 'resetPassword']);
+});
+Route::group(['middleware' => 'api'], function () {
+
     Route::get('/videos', [VideosController::class, 'videos']);
     Route::get('/videos/{id}', [VideosController::class, 'video']);
+    Route::get('/creators', [CreatorsController::class, 'creators']);
+    Route::get('/creators/{id}', [CreatorsController::class, 'creator']);
     Route::get('/categories', [CategoriesController::class, 'categories']);
-    Route::get('/bookmarks', [UsersController::class, 'videoBookmarks']);
-    Route::post('/bookmark', [UsersController::class, 'bookmarkVideo']);
+
+    Route::group(['middleware' => 'jwt.auth'], function () {
+        Route::get('/profile', [AuthController::class, 'profile']);
+        Route::post('change-password', [AuthController::class, 'changePassword']);
+        Route::get('/bookmarks', [UsersController::class, 'videoBookmarks']);
+        Route::post('/bookmark', [UsersController::class, 'bookmarkVideo']);
+    });
 });
