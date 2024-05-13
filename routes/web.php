13d2baa331e308b01729e9def2842b69ca377bda
Ansel Melly <ansel@anselmelly.com>
2024-05-13 18:11:25 +0300

    change url for admin

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\{
 };
 
 Route::get('/', function () {
-    return \redirect()->to('/login');
+    return \redirect()->to('/admin/login');
 });
 Route::get('/mail',function(){
     $user = User::whereId(5)->first();
