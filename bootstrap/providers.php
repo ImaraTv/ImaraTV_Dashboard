2179d18d7b1641e2b3da1240752346e88a4156af
renjeroham <renjeroham.vargas@gmail.com>
2024-05-22 09:22:56 -0400

    Complete Sponsor Profile Banner

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FAuthServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AuthPanelProvider::class,
     App\Providers\Filament\FilmTopicsPanelProvider::class,
     App\Providers\Filament\SuperAdminPanelProvider::class,
