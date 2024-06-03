aef1a24981e2905c82497f996f1ebce8ab103c8a
Ansel Melly <ansel@anselmelly.com>
2024-06-03 20:19:45 +0300

    fix email formatting

--- resources/views/emails/user-reset-password.blade.php
+++ resources/views/emails/user-reset-password.blade.php
@@ -1,39 +1,39 @@
 <x-mail::message>
 
-    # Hello!
+# Hello!
 
-    You requested to reset your password. Click on the link below to reset.
+You requested to reset your password. Click on the link below to reset.
 
-    if you did not request a reset password, you can ignore this email
-
-
-    @isset($actionText)
-    <?php
-    $color = match ($level) {
-        'success', 'error' => $level,
-        default => 'primary',
-    };
-    ?>
-    <x-mail::button :url="$actionUrl" :color="$color">
-        {{ $actionText }}
-    </x-mail::button>
-    @endisset
+if you did not request a reset password, you can ignore this email
 
 
-    Regards,<br>
-    {{ config('app.name') }}
+@isset($actionText)
+<?php
+$color = match ($level) {
+    'success', 'error' => $level,
+    default => 'primary',
+};
+?>
+<x-mail::button :url="$actionUrl" :color="$color">
+    {{ $actionText }}
+</x-mail::button>
+@endisset
 
 
-   
-    @isset($actionText)
-    <x-slot name="subcopy">
-        @lang(
-        "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-        'into your web browser:',
-        [
-        'actionText' => $actionText,
-        ]
-        ) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-    </x-slot>
-    @endisset
+Regards,<br>
+{{ config('app.name') }}
+
+
+
+@isset($actionText)
+<x-slot name="subcopy">
+    @lang(
+    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
+    'into your web browser:',
+    [
+    'actionText' => $actionText,
+    ]
+    ) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+</x-slot>
+@endisset
 </x-mail::message>
