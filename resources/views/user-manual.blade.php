4ec2c6b1ad7b5f8129d4011014270a6c1ecfec32
renjeroham <renjeroham.vargas@gmail.com>
2024-07-01 14:44:34 -0400

    Added User Manual for users.
    Added a button that displays the user manual at the bottom of the dashboard page.

---
+++ resources/views/user-manual.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title id="#top">Imara.TV User Manual</title>
+        <h1>Imara.TV User Manual</h1>
+        <a href="#creator">Creator User Manual</a>
+        <span> | </span>
+        <a href="#sponsor">Sponsor User Manual</a>
+        <span> | </span>
+        <a href="#admin">Admin User Manual</a>
+    </head>
+
+    <body>
+        <div id="creator-manual">
+            <h2 id="creator">Creator User Manual</h2>
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide1.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide2.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide3.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide4.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide5.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide6.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide7.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide8.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide9.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Creator Dashboard User Manual\Slide10.JPG')}}" alt="">
+            <a href="#top"><h3>Back to Top</h3></a>
+
+        </div>
+        <div id="sponsor-manual">
+            <h2 id="sponsor">Sponsor User Manual</h2>
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide1.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide2.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide3.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide4.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide5.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide6.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide7.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide8.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide9.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide10.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide11.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara TV Sponsor Dashboard\Slide12.JPG')}}" alt="">
+            <a href="#top"><h3>Back to Top</h3></a>
+
+
+        </div>
+        <div id="admin-manual">
+            <h2 id="admin">Admin User Manual</h2>
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide1.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide2.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide3.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide4.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide5.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide6.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide7.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide8.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide9.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide10.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide11.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide12.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide13.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide14.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide15.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide16.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide17.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide18.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide19.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide20.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide21.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide22.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide23.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide24.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide25.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide26.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide27.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide28.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide29.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide30.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide31.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide32.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide33.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide34.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide35.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide36.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide37.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide38.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide39.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide40.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide41.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide42.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide43.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide44.JPG')}}" alt="">
+            <img src="{{asset('/images/Imara Tv Admin dashboard User Manual\Slide45.JPG')}}" alt="">
+            <a href="#top"><h3>Back to Top</h3></a>
+        </div>
+        
+
+    </body>
+</html>
